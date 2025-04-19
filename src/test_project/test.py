596a95edf0c7b92910fbc997c5867ce103d5b899
Jayakrishna Alapati <jayakrishna@Jayakrishnas-Mac-mini.local>
2025-04-19 13:45:26 +0530

    test jenkins

--- src/test_project/test.py
+++ src/test_project/test.py
@@ -11,19 +11,6 @@ def add_numbers(a, b):
     """
     return a + b
 
-def subtract_numbers(a, b):
-    """
-    @brief Subtracts the second number from the first and returns the result.
-    
-    @param a The first number (int or float)
-    @param b The second number (int or float)
-    @return The result of the subtraction (int or float)
-    
-    @details This function subtracts the second number from the first number
-    and returns the result. It can handle both integer and floating-point numbers.
-    """
-    return a - b
-
 def main():
     """
     @brief Main function to demonstrate the add_numbers and subtract_numbers functions.
@@ -37,9 +24,6 @@ def main():
     
     sum_result = add_numbers(num1, num2)
     print(f"The sum of {num1} and {num2} is {sum_result}")
-    
-    subtraction_result = subtract_numbers(num1, num2)
-    print(f"The result of subtracting {num2} from {num1} is {subtraction_result}")
 
 if __name__ == "__main__":
     main()
